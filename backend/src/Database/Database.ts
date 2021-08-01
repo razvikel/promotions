c0bb13a33fafccb88ded8060a7d5fb317c49db16
Raz Vikelman <razvikel@gmail.com>
2021-08-01 19:19:24 +0300

    change database

--- backend/src/Database/Database.ts
+++ backend/src/Database/Database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export class Database {
     public static async connect() {
-        await mongoose.connect('mongodb://localhost/moon', { useNewUrlParser: true });
+        await mongoose.connect('mongodb+srv://raz:raz@ztube.ujqsy.mongodb.net/moon?retryWrites=true&w=majority', { useNewUrlParser: true });
         console.log('connected to db');
     }
 }
