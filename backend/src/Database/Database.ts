b00d70015b6d26a647ccff09343599ca07a50a88
Raz Vikelman <razvikel@gmail.com>
2021-08-03 10:42:55 +0300

    recreate database

--- backend/src/Database/Database.ts
+++ backend/src/Database/Database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export class Database {
     public static async connect() {
-        await mongoose.connect('mongodb+srv://raz:raz@ztube.ujqsy.mongodb.net/moon?retryWrites=true&w=majority', { useNewUrlParser: true });
+        await mongoose.connect('mongodb+srv://raz:raz@cluster0.ujqsy.mongodb.net/moon?retryWrites=true&w=majority', { useNewUrlParser: true });
         // await mongoose.connect('mongodb://localhost/moon', { useNewUrlParser: true });
         console.log('connected to db');
     }
